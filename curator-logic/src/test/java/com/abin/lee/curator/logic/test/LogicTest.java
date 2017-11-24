85d0e9935c63a534c91208b4fd6be8bec2bb3f91
zondahuman <zondahuman@gmail.com>
2017-11-24 10:44:42 +0800

    add

--- curator-logic/src/test/java/com/abin/lee/curator/logic/test/LogicTest.java
+++ curator-logic/src/test/java/com/abin/lee/curator/logic/test/LogicTest.java
@@ -1,5 +1,10 @@
 package com.abin.lee.curator.logic.test;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
  * Created by abin on 2017/11/23 20:21.
  * curator-svr
@@ -7,7 +12,18 @@ package com.abin.lee.curator.logic.test;
  */
 public class LogicTest {
 
+    public static void main(String[] args) {
+        
+    }
 
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        Map<K, V> result = new LinkedHashMap<>();
+        Stream<Map.Entry<K, V>> st = map.entrySet().stream();
+
+        st.sorted(Comparator.comparing(e -> e.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
+
+        return result;
+    }
 
 
 }
