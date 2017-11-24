1e6a06725a0defe6917a13a7311777c3401a6e59
zondahuman <zondahuman@gmail.com>
2017-11-24 19:40:47 +0800

    add

--- curator-logic/src/test/java/com/abin/lee/curator/logic/test/LogicTest.java
+++ curator-logic/src/test/java/com/abin/lee/curator/logic/test/LogicTest.java
@@ -1,11 +1,10 @@
 package com.abin.lee.curator.logic.test;
 
 import com.google.common.collect.Maps;
+import org.apache.commons.collections.MapUtils;
+import org.junit.Test;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -25,19 +24,66 @@ public class LogicTest {
         frequence1.put("abin9", 9);
         System.out.println("-------------------------------------");
 
-       String result = get(frequence1, 2);
+        String result = get(frequence1, 2);
         System.out.println("result=" + result);
 
     }
 
-    public static String get(Map<String, Integer> frequence1, Integer num){
+    @Test
+    public void test1() {
+        Map<String, Integer> frequence = Maps.newHashMap();
+        frequence.put("abin10", 10);
+        frequence.put("abin6", 6);
+        frequence.put("abin3", 3);
+        frequence.put("abin8", 8);
+//        frequence.put("abin1", 1);
+//        frequence.put("abin2", 2);
+//        frequence.put("abin4", 4);
+//        frequence.put("abin7", 7);
+//        frequence.put("abin5", 5);
+//        frequence.put("abin9", 9);
+        System.out.println("-------------------------------------");
+
+        Map<String, Integer> result = get1(frequence, 5);
+        System.out.println("result=" + result);
+    }
+
+
+    public static Map<String, Integer> get1(Map<String, Integer> frequence1, Integer num) {
+        Map<String, Integer> result = sortByValue(frequence1);
+        Map<String, Integer> result1 = Maps.newLinkedHashMap();
+        if (MapUtils.isNotEmpty(result)) {
+
+            if (result.size() >= num) {
+                AtomicInteger increase = new AtomicInteger(0);
+                for (Iterator<Map.Entry<String, Integer>> iterator = result.entrySet().iterator(); iterator.hasNext(); ) {
+                    Map.Entry<String, Integer> entry = iterator.next();
+                    if (increase.get() == num) {
+                        return result1;
+                    } else {
+                        result1.put(entry.getKey(), entry.getValue());
+                        increase.getAndIncrement();
+                    }
+                }
+            } else {
+                for (Iterator<Map.Entry<String, Integer>> iterator = result.entrySet().iterator(); iterator.hasNext(); ) {
+                    Map.Entry<String, Integer> entry = iterator.next();
+                    result1.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        return result1;
+    }
+
+
+    public static String get(Map<String, Integer> frequence1, Integer num) {
         Map<String, Integer> result1 = sortByValue(frequence1);
         AtomicInteger increase = new AtomicInteger(0);
         String param = "";
-        for(Iterator<Map.Entry<String, Integer>> iterator = result1.entrySet().iterator(); iterator.hasNext();){
+        for (Iterator<Map.Entry<String, Integer>> iterator = result1.entrySet().iterator(); iterator.hasNext(); ) {
             Map.Entry<String, Integer> entry = iterator.next();
-            System.out.println("key="+entry.getKey() + ", value="+entry.getValue());
-            if(frequence1.size() >= num) {
+            System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
+            if (frequence1.size() >= num) {
                 if (increase.get() == num) {
                     param = entry.getKey();
                     return param;
@@ -53,7 +99,8 @@ public class LogicTest {
         Map<K, V> result = new LinkedHashMap<>();
         Stream<Map.Entry<K, V>> st = map.entrySet().stream();
 
-        st.sorted(Comparator.comparing(e -> e.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
+        //        st.sorted(Comparator.comparing(e -> e.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
+        st.sorted((c1,c2)-> c2.getValue().compareTo(c1.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
 
         return result;
     }
